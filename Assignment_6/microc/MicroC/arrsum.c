ac8d9407b862209d5d305160befa00b4025af712
DHLarsen <daniel.holm.larsen@gmail.com>
2024-10-10 11:34:15 +0200

    7.2.1

---
+++ Assignment_6/microc/MicroC/arrsum.c
@@ -0,0 +1,28 @@
+// Computes and returns the sum of the first n elements of arr
+
+void arrsum(int n, int arr[], int *sump)
+{
+	int i;
+	i = 0;
+	int sum;
+	sum = 0;
+	while (i < n)
+	{
+	sum = sum + arr[i];
+	i = i +1;
+	}
+	*sump = sum;
+}
+
+void main()
+{
+	int arr[4];
+	arr[0] = 7;
+	arr[1] = 13;
+	arr[2] = 9;
+	arr[3] = 8;
+	int p;
+	arrsum(4, arr, &p);
+	print p;
+}
+
